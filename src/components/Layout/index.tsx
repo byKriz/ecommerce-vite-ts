04e71896209609f32a3a9c0b0d23e28b9799227a
bykriz <cristianesquivelh@hotmail.com>
2023-05-11 18:14:13 -0400

    formateando el codigo

--- src/components/Layout/index.tsx
+++ src/components/Layout/index.tsx
@@ -2,11 +2,12 @@ import React from "react";
 
 interface Prop extends React.HTMLAttributes<HTMLDivElement> {
   children: React.ReactNode;
-  
 }
 
 export const Layout = ({ children, ...layoutProps }: Prop) => {
-  return <div className="flex flex-col items-center mt-20" {...layoutProps}>
-    {children}
-  </div>;
+  return (
+    <div className="flex flex-col items-center mt-20" {...layoutProps}>
+      {children}
+    </div>
+  );
 };
