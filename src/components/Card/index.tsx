506df3a0e1e489a367599d767aead8d1c5a4bcf0
bykriz <cristianesquivelh@hotmail.com>
2023-05-11 01:42:46 -0400

    consumiendo una api de productos

--- src/components/Card/index.tsx
+++ src/components/Card/index.tsx
@@ -1,11 +1,11 @@
 import React from "react";
+import { Product } from "../../Interfaces/Product";
 
-interface Props extends React.HTMLAttributes<HTMLDivElement> {}
+interface Props extends React.HTMLAttributes<HTMLDivElement> {
+  product: Product;
+}
 
-const imageTestLink: string =
-  "https://images.pexels.com/photos/1037992/pexels-photo-1037992.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1";
-
-export const Card = ({ ...cardProps }: Props): JSX.Element => {
+export const Card = ({ product, ...cardProps }: Props): JSX.Element => {
   return (
     <div
       className="bg-white cursor-pointer w-56 h-60 rounded-lg"
@@ -13,16 +13,20 @@ export const Card = ({ ...cardProps }: Props): JSX.Element => {
     >
       <figure className="relative mb-2 w-full h-4/5">
         <span className="absolute bottom-0 left-0 m-2 bg-white/60 rounded-lg text-black text-xs px-3 py-0.5">
-          Electronics
+          {product.category}
         </span>
-        <img className="w-full h-full object-cover rounded-lg" src={imageTestLink} alt="nombre del producto"/>
+        <img
+          className="w-full h-full object-cover rounded-lg"
+          src={product.image}
+          alt="nombre del producto"
+        />
         <div className="absolute top-1 right-1 flex justify-center items-center bg-white w-6 h-6 rounded-full m-2 p-1">
           +
         </div>
       </figure>
       <p className="flex justify-between">
-        <span className="text-sm font-light">Headphone</span>
-        <span className="text-lg font-medium">$30</span>
+        <span className="text-sm font-light">{product.title}</span>
+        <span className="text-lg font-medium">{product.price}$</span>
       </p>
     </div>
   );
--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -1,11 +1,25 @@
+import { useEffect, useState } from "react";
 import { Card } from "../../components/Card";
 import { Layout } from "../../components/Layout";
+import { Product } from "../../Interfaces/Product";
 
 export const Home = () => {
+  const [products, setProducts] = useState<Product[]>([]);
+
+  useEffect(() => {
+    fetch("https://fakestoreapi.com/products")
+      .then((resp) => resp.json())
+      .then((data) => setProducts(data))
+      .catch((error) => console.error(error));
+  }, []);
+
   return (
     <Layout>
-      Hola
-      <Card />
+      <div className="grid gap-4 grid-cols-4 w-full max-w-screen-lg">
+        {products.map((product) => (
+          <Card product={product} key={product.id} />
+        ))}
+      </div>
     </Layout>
   );
 };
