f76607527ac9cbbcc4fc709ad19f2b7706dc33f3
bykriz <cristianesquivelh@hotmail.com>
2023-05-20 18:20:19 -0400

    agregando un buscador por titulo

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -1,24 +1,41 @@
-import { useEffect, useState } from "react";
+import { ChangeEvent, useContext, useState } from "react";
 import { Card } from "../../components/Card";
 import { Layout } from "../../components/Layout";
-import { Product } from "../../Interfaces/Product";
 import { ProductDetails } from "../../components/ProductDetail";
 import { CheckouSideMenu } from "../../components/CheckoutSideMenu";
+import { ShoppingCartContext } from "../../Context/ShoppingCartContext";
+
+type ChangeEventType = ChangeEvent<HTMLInputElement>;
 
 export const Home = () => {
-  const [products, setProducts] = useState<Product[]>([]);
+  const [search, setSearch] = useState<string>("");
+  const context = useContext(ShoppingCartContext);
+  const products = context.products;
 
-  useEffect(() => {
-    fetch("https://api.escuelajs.co/api/v1/products")
-      .then((resp) => resp.json())
-      .then((data) => setProducts(data))
-      .catch((error) => console.error(error));
-  }, []);
+  const handleSearch = (e: ChangeEventType) => {
+    setSearch(e.target.value.toLocaleLowerCase());
+  };
+
+  const filteredData =
+    search.length === 0
+      ? products
+      : products.filter((item) =>
+          item.title.toLocaleLowerCase().includes(search)
+        );
 
   return (
     <Layout>
+      <div className="flex items-center justify-center relative w-80 mb-4">
+        <h1 className="font-medium text-xl">Exclusive Products</h1>
+      </div>
+      <input
+        type="text"
+        placeholder="Search a product"
+        className="mb-4 text-center rounded-lg border border-black w-80 p-4"
+        onChange={(e: ChangeEventType) => handleSearch(e)}
+      />
       <div className="grid gap-4 grid-cols-4 w-full max-w-screen-lg">
-        {products.map((product) => (
+        {filteredData.map((product) => (
           <Card product={product} key={product.id} />
         ))}
       </div>
