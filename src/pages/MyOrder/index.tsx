c350402567f2e6b857f716674c374a55c9dbd89c
bykriz <cristianesquivelh@hotmail.com>
2023-05-20 00:14:17 -0400

    refactorizando la funcion viewOrder

--- src/pages/MyOrder/index.tsx
+++ src/pages/MyOrder/index.tsx
@@ -5,6 +5,12 @@ import { OrderCard } from "../../components/OrderCard";
 import { Link } from "react-router-dom";
 import { ChevronLeftIcon } from "@heroicons/react/24/outline";
 import { useParams } from "react-router-dom";
+import { CartItem } from "../../Interfaces/IteamCart";
+
+interface ViewOrder {
+  products: CartItem[] | undefined;
+  totalPrice: number | undefined;
+}
 
 export const MyOrder = () => {
   const context = useContext(ShoppingCartContext);
@@ -18,25 +24,28 @@ export const MyOrder = () => {
 
   const params = useParams();
 
-  const viewOrder = (): JSX.Element[] | null | undefined => {
+  const viewOrder = (): ViewOrder | null => {
     if (existOrder()) {
+      const lastOrder = context.orders.slice(-1)[0];
+      const orderById = context.orders.filter(
+        (order) => order.id === params.id
+      )[0];
+
       if (params.id === "last") {
-        return context.orders
-          .slice(-1)[0]
-          .products?.map((product) => (
-            <OrderCard product={product} key={product.id} />
-          ));
+        const products = lastOrder.products;
+        const totalPrice = lastOrder.totalPrice;
+        return { products, totalPrice };
       } else {
-        return context.orders
-          .filter((order) => order.id === params.id)[0]
-          .products?.map((product) => (
-            <OrderCard product={product} key={product.id} />
-          ));
+        const products = orderById.products;
+        const totalPrice = orderById ? orderById.totalPrice : 0;
+        return { products, totalPrice };
       }
     } else {
       return null;
     }
   };
+
+  const order = viewOrder();
 
   return (
     <Layout>
@@ -46,7 +55,12 @@ export const MyOrder = () => {
         </Link>
         <h1>MY ORDER</h1>
       </div>
-      <div className="flex flex-col gap-2 w-80">{viewOrder()}</div>
+      <div className="flex flex-col gap-2 w-80">
+        {order?.products?.map((item) => (
+          <OrderCard product={item} key={item.id} />
+        ))}
+      </div>
+      <span>Total Price: ${order?.totalPrice}</span>
     </Layout>
   );
 };
